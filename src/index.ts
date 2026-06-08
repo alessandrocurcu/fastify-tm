d5a5d7064cb73ac8d68f225711c4f4221bb23e87
Alessandro Curcu <aleqrq@gmail.com>
2026-06-08 08:33:55 +0200

    feat: aggiungi rotte utenti e configurazione Fastify con Zod

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,15 @@
+import sensible from '@fastify/sensible';
+import { serializerCompiler, validatorCompiler } from '@fastify/type-provider-zod';
 import Fastify from 'fastify';
+import usersRoutes from './routes/users.ts';
 
 const app = Fastify({ logger: true });
+
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
+
+await app.register(sensible);
+await app.register(usersRoutes);
 
 app.get('/health', async () => {
   return { status: 'ok' };
---
+++ src/routes/users.ts
@@ -0,0 +1,52 @@
+/* eslint-disable prefer-arrow-callback */
+import type { FastifyPluginAsyncZod } from '@fastify/type-provider-zod';
+import { z } from 'zod';
+
+const paramsSchema = z.object({
+  id: z.coerce.number().positive(),
+});
+
+const userSchema = z.object({
+  id: z.number(),
+  email: z.string(),
+  // name: z.string(),
+  created_at: z.string(),
+});
+
+const usersRoutes: FastifyPluginAsyncZod = async (fastify) => {
+  fastify.get(
+    '/users',
+    { schema: { response: { 200: z.array(userSchema) } } },
+    async function listUsers() {
+      // return fastify.db.prepare('SELECT * FROM users').all() as z.infer<typeof userSchema>[];
+    },
+  );
+
+  fastify.get(
+    '/users/:id',
+    {
+      schema: {
+        params: paramsSchema,
+        response: { 200: userSchema },
+      },
+    },
+    async function getUser(request, reply) {
+      const user: z.infer<typeof userSchema> = {
+        id: 2,
+        email: 'Ale',
+        created_at: '',
+      };
+      // const user2 = fastify.db
+      //   .prepare('SELECT * FROM users WHERE id = ?')
+      //   .get(request.params.id) as z.infer<typeof userSchema> | undefined;
+
+      if (!user) {
+        return reply.notFound(`User ${request.params.id} not found`);
+      }
+
+      return user;
+    },
+  );
+};
+
+export default usersRoutes;
